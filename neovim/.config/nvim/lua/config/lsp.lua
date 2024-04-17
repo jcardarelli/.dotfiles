ff68cc3951296052ba015bbfe13bb502e06ec005
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-04-17 14:24:08 -0700

    Change opts.border to border instead of single and remove max_width

--- neovim/.config/nvim/lua/config/lsp.lua
+++ neovim/.config/nvim/lua/config/lsp.lua
@@ -27,8 +27,7 @@ local border = {
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 	opts = opts or {}
-	opts.border = opts.border or "single"
-	opts.max_width = opts.max_width or 80
+	opts.border = opts.border or border
 	return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
