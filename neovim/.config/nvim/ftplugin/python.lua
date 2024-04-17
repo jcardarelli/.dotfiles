dba30ec3605836793ab279163210fb1856593123
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-04-16 23:27:10 -0700

    Set vim.opt.keywordprg to pydoc3

--- neovim/.config/nvim/ftplugin/python.lua
+++ neovim/.config/nvim/ftplugin/python.lua
@@ -2,3 +2,7 @@
 vim.opt.expandtab = true
 
 vim.opt.textwidth = 79
+
+-- Set keyword program for docs on hover (K)
+-- https://www.reddit.com/r/vim/comments/w2p6mv/comment/igsl8ln/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+vim.opt.keywordprg = "pydoc3"
