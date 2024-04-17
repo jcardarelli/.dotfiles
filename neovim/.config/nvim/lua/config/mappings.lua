4a6f866107f3901ac2cd54b153022bef9e8845c9
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-04-16 23:21:02 -0700

    Reformat single quotes into double quotes

--- neovim/.config/nvim/lua/config/mappings.lua
+++ neovim/.config/nvim/lua/config/mappings.lua
@@ -85,18 +85,18 @@ vim.cmd("nnoremap <silent> <S-\\><C-\\> :ToggleTerm direction=horizontal size=90
 -- https://github.com/akinsho/toggleterm.nvim#terminal-window-mappings
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-	vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-	vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-	vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
 	-- this one is excluded so that Ctrl+l (clear) works as expected while in the terminal window
 	-- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-	vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 -- Git
 -- mappings for using git in the built-in terminal
