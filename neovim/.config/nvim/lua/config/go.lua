901a92935f4589f23fc732877ad5ba73a232d575
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-04-24 10:58:36 -0700

    Enable luasnip snippets for go.nvim plugin

--- neovim/.config/nvim/lua/config/go.lua
+++ neovim/.config/nvim/lua/config/go.lua
@@ -7,6 +7,6 @@ require("go").setup({
 	run_in_floaterm = true, -- set to true to run in float window. :GoTermClose closes the floatterm
 	-- float term recommend if you use richgo/ginkgo with terminal color
 	trouble = true, -- true: use trouble to open quickfix
-	luasnip = false, -- enable included luasnip snippets. you can also disable while add lua/snips folder to luasnip load
+	luasnip = true, -- enable included luasnip snippets. you can also disable while add lua/snips folder to luasnip load
 	--  Do not enable this if you already added the path, that will duplicate the entries
 })
