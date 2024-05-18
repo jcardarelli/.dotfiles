4bde62c781aee5c96d99f23bf312e02932368531
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-18 15:15:59 -0700

    Add comment explaining which bufferline plugin to use post 0.10

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,4 +1,6 @@
 return {
+	-- https://www.reddit.com/r/neovim/comments/1ctx4z7/comment/l4fvgjs/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+	-- This replaces akinsho/bufferline.nvim, which seems to have broken with neovim 0.10
 	"Theyashsawarkar/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
