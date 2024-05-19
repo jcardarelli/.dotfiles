5c62aa67cd7d5f3d1fa7d09f13be0e2e39f6746f
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-19 15:08:05 -0700

    Re-enable lspkind for pictograms in nvim-cmp menus

--- neovim/.config/nvim/lua/config/nvim-cmp.lua
+++ neovim/.config/nvim/lua/config/nvim-cmp.lua
@@ -7,7 +7,7 @@ end
 -- Setup nvim-cmp
 --------------------------------------------------------------------------------
 local cmp = require("cmp")
--- local lspkind = require("lspkind")
+local lspkind = require("lspkind")
 cmp.setup({
 	enabled = function()
 		-- Disable nvim-cmp in Telescope's buffer
@@ -35,20 +35,18 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
-	-- commented out on 2024-04-13
-	-- do I need this?
-	-- formatting = {
-	-- 	format = lspkind.cmp_format({
-	-- 		mode = "symbol_text", -- show symbol and text annotations
-	-- 		maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-	-- 		ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-	-- 		-- The function below will be called before any actual modifications from lspkind
-	-- 		-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-	-- 		before = function(entry, vim_item)
-	-- 			return vim_item
-	-- 		end,
-	-- 	}),
-	-- },
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text", -- show symbol and text annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
