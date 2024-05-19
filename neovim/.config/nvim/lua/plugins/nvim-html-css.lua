f8ad43c7e6751f2371fcf2d946479652c25685a0
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-18 17:18:52 -0700

    Lazy load nvim-html-css plugin when ft is html or css

--- neovim/.config/nvim/lua/plugins/nvim-html-css.lua
+++ neovim/.config/nvim/lua/plugins/nvim-html-css.lua
@@ -17,10 +17,11 @@ return {
 		"Jezda1337/nvim-html-css",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
-			"nvim-lua/plenary.nvim"
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
 			require("html-css"):setup()
-		end
-	}
+		end,
+		ft = { "html", "css" },
+	},
 }
