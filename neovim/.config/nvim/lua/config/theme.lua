e78a012f8766cf797d3b688ccd1e52930be9f9c8
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-19 13:40:00 -0700

    Move lualine config to plugin definition

--- neovim/.config/nvim/lua/config/theme.lua
+++ neovim/.config/nvim/lua/config/theme.lua
@@ -33,35 +33,6 @@ vim.api.nvim_cmd({
 }, {})
 
 --------------------------------------------------------------------------------
--- lualine setup
---------------------------------------------------------------------------------
-require("lualine").setup({
-	options = {
-		theme = "tokyonight",
-		component_separators = "",
-		disabled_filetypes = {
-			"Lazy",
-			"NvimTree",
-			"sagaoutline",
-			"toggleterm",
-		},
-
-		-- If current filetype is in this list it'll always be drawn as inactive
-		-- statusline and the last window will be drawn as active statusline. For
-		-- example if you don't want statusline of your file tree / sidebar window
-		-- to have active statusline you can add their filetypes here.
-		ignore_focus = {
-			"dap-repl",
-			"dapui_breakpoints",
-			"dapui_console",
-			"dapui_scopes",
-			"dapui_stacks",
-			"dapui_watches",
-		},
-	},
-})
-
---------------------------------------------------------------------------------
 -- Buffer tab bars
 --------------------------------------------------------------------------------
 require("bufferline").setup({
--- neovim/.config/nvim/lua/plugins/tokyonight.lua
+++ neovim/.config/nvim/lua/plugins/tokyonight.lua
@@ -3,7 +3,33 @@ return {
 	"folke/tokyonight.nvim",
 	{
 		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+					component_separators = "",
+					disabled_filetypes = {
+						"Lazy",
+						"NvimTree",
+						"sagaoutline",
+						"toggleterm",
+					},
+
+					-- If current filetype is in this list it'll always be drawn as inactive
+					-- statusline and the last window will be drawn as active statusline. For
+					-- example if you don't want statusline of your file tree / sidebar window
+					-- to have active statusline you can add their filetypes here.
+					ignore_focus = {
+						"dap-repl",
+						"dapui_breakpoints",
+						"dapui_console",
+						"dapui_scopes",
+						"dapui_stacks",
+						"dapui_watches",
+					},
+				},
+			})
+		end,
 		dependencies = { "nvim-tree/nvim-web-devicons", opt = false },
 	},
-	lazy=true,
 }
