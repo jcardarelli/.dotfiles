7470ea6130b100353b7947a779536e2117d33d3f
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-20 13:40:22 -0700

    Remove comments in theme config file

--- neovim/.config/nvim/lua/config/theme.lua
+++ neovim/.config/nvim/lua/config/theme.lua
@@ -1,8 +1,6 @@
---------------------------------------------------------------------------------
--- Current theme settings
---------------------------------------------------------------------------------
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
+
 -- https://github.com/neovim/neovim/issues/18201#issuecomment-1382075049
 vim.api.nvim_cmd({
 	cmd = "colorscheme",
