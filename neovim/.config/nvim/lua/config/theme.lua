37fd642efc76dab6870ff2705e5be01461bdcc90
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-20 13:11:04 -0700

    Move bufferline config to plugin def

--- neovim/.config/nvim/lua/config/theme.lua
+++ neovim/.config/nvim/lua/config/theme.lua
@@ -8,30 +8,3 @@ vim.api.nvim_cmd({
 	cmd = "colorscheme",
 	args = { "tokyonight" },
 }, {})
-
---------------------------------------------------------------------------------
--- Buffer tab bars
---------------------------------------------------------------------------------
-require("bufferline").setup({
-	options = {
-		offsets = {
-			{
-				filetype = "NvimTree",
-				highlight = "Directory",
-				separator = true,
-				text = "",
-				text_align = "left",
-			},
-		},
-		always_show_bufferline = true,
-		separator_style = "thin",
-		numbers = "buffer_id",
-		-- indicator icons
-		show_tab_indicators = true,
-		buffer_close_icon = "󰅖",
-		modified_icon = "●",
-		close_icon = "",
-		left_trunc_marker = "",
-		right_trunc_marker = "",
-	},
-})
--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -4,10 +4,30 @@ return {
 	"Theyashsawarkar/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	opts = {
-		options = {
-			mode = "tabs",
-			separator_style = "slant",
-		},
-	},
+	config = function()
+		require("bufferline").setup({
+			options = {
+				offsets = {
+					{
+						filetype = "NvimTree",
+						highlight = "Directory",
+						separator = true,
+						text = "",
+						text_align = "left",
+					},
+				},
+				always_show_bufferline = true,
+				mode = "tabs",
+				separator_style = "thin",
+				numbers = "buffer_id",
+				-- indicator icons
+				show_tab_indicators = true,
+				buffer_close_icon = "󰅖",
+				modified_icon = "●",
+				close_icon = "",
+				left_trunc_marker = "",
+				right_trunc_marker = "",
+			},
+		})
+	end,
 }
