4bfde666a61ca165a089ff1bcf392360fe88d131
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-20 13:13:57 -0700

    Add more context about why bufferline fork

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,8 @@
 return {
 	-- https://www.reddit.com/r/neovim/comments/1ctx4z7/comment/l4fvgjs/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 	-- This replaces akinsho/bufferline.nvim, which seems to have broken with neovim 0.10
+	-- Replace this plugin with the non-forked version once this fix is introduced:
+	-- https://github.com/akinsho/bufferline.nvim/compare/main...Theyashsawarkar:bufferline.nvim:main
 	"Theyashsawarkar/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
