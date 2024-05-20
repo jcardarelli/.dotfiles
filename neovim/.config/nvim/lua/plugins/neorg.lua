2f3b0dbae062f461653a866a1762ce900991e64a
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-20 12:49:15 -0700

    Only load neorg plugin on Neorg command

--- neovim/.config/nvim/lua/plugins/neorg.lua
+++ neovim/.config/nvim/lua/plugins/neorg.lua
@@ -2,8 +2,9 @@ return {
 	"nvim-neorg/neorg",
 	build = ":Neorg sync-parsers",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = "Neorg",
 	config = function()
-		require("neorg").setup {
+		require("neorg").setup({
 			load = {
 				["core.defaults"] = {}, -- Loads default behaviour
 				["core.concealer"] = {}, -- Adds pretty icons to your documents
@@ -16,6 +17,6 @@ return {
 					},
 				},
 			},
-		}
+		})
 	end,
 }
