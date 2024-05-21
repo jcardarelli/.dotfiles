5a668a4018c18fd9081602135e0cc0fd5a925e22
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-21 12:12:18 -0700

    Remove old comments in bufferline.lua

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,5 @@
--- https://www.reddit.com/r/neovim/comments/1ctx4z7/comment/l4fvgjs/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
--- This replaces akinsho/bufferline.nvim, which seems to have broken with neovim 0.10
--- Replace this plugin with the non-forked version once this fix is introduced:
--- https://github.com/akinsho/bufferline.nvim/compare/main...Theyashsawarkar:bufferline.nvim:main
--- Watch the get_component_size function for updates:
--- https://github.com/akinsho/bufferline.nvim/blob/main/lua/bufferline/ui.lua#L119
 return {
 	"akinsho/bufferline.nvim",
-	-- "Theyashsawarkar/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
 	init = function()
