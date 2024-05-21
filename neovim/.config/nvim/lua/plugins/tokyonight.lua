1136b2dc5112fd196a068550f3d17f729752a9c9
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-21 12:14:36 -0700

    Disable terminal_colors in tokyonight to no effect

--- neovim/.config/nvim/lua/plugins/tokyonight.lua
+++ neovim/.config/nvim/lua/plugins/tokyonight.lua
@@ -6,7 +6,7 @@ return {
 			style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 			light_style = "day", -- The theme is used when the background is set to light
 			transparent = true, -- Enable this to disable setting the background color
-			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+			terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
 			styles = {
 				-- Style to be applied to different syntax groups
 				-- Value is any valid attr-list value for `:help nvim_set_hl`
