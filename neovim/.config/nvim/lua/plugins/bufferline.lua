4da4359c83e2842119264a2dd634dfe5d7fcd9f7
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-22 12:14:41 -0700

    Enable LSP diagnostic error or warning for bufferline

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -18,6 +18,15 @@ return {
 				always_show_bufferline = true,
 				separator_style = "thin",
 				numbers = "buffer_id",
+				diagnostics = "nvim_lsp",
+				--- count is an integer representing total count of errors
+				--- level is a string "error" | "warning"
+				--- this should return a string
+				--- Don't get too fancy as this function will be executed a lot
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or ""
+					return " " .. icon .. count
+				end,
 				-- indicator icons
 				show_tab_indicators = true,
 				buffer_close_icon = "",
