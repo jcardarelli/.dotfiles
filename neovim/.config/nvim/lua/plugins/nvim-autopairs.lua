9046c4ae4e9b26290a8c07cf34220c9aee768b63
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-21 18:05:12 -0700

    Format lua/plugins/nvim-autopairs.lua

--- neovim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ neovim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("nvim-autopairs").setup({
 			-- if next character is a close pair and it doesn't have an open pair in same line, then it will not add a close pair
-			enable_check_bracket_line = true
+			enable_check_bracket_line = true,
 		})
 	end,
 }
