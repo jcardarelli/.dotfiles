d23ca9a042f78a91bdd08d0e3768c076653bc0a2
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-05-23 21:43:31 -0700

    Add nvim-ts-autotag dep for nvim-autopairs

--- neovim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ neovim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,18 @@
 return {
 	"windwp/nvim-autopairs",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		init = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+			})
+		end,
+	}, -- html autopairs
 	config = function()
 		require("nvim-autopairs").setup({
 			-- if next character is a close pair and it doesn't have an open pair in same line, then it will not add a close pair
