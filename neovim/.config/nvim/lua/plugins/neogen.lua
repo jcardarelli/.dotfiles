6aea1b07fa303fb687e797127d8f75d6e5cfa8e2
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-06-27 13:04:19 -0700

    Remove useless comment

--- neovim/.config/nvim/lua/plugins/neogen.lua
+++ neovim/.config/nvim/lua/plugins/neogen.lua
@@ -2,7 +2,7 @@ return {
 	"danymat/neogen",
 	init = function()
 		require("neogen").setup({
-			enabled = true, --if you want to disable Neogen
+			enabled = true,
 			input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
 			-- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
 		})
