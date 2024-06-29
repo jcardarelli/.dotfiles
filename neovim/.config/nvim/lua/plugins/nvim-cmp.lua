ea09f17b27c8d450c1b72c22ee839d5e3bd7b055
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-06-28 21:30:28 -0700

    Split folke plugins into separate files in plugins dir

--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,4 +1,14 @@
 return {
+	{ -- optional completion source for require statements and module annotations
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+	},
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-cmdline",
 	"hrsh7th/cmp-nvim-lsp",
