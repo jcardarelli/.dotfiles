eb4adea95110bce758641a74e9fb9de7b1e3596a
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-08-22 00:19:19 -0700

    Use prettier for css formatting instead of stylelint

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -16,7 +16,7 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-				css = { "stylelint" },
+				css = { "prettier" },
 				go = { "gofumpt", "goimports", "golines" },
 				html = { "htmlbeautifier" },
 				javascript = { "prettierd", "prettier" },
