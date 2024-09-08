2b9e548ee8a189d6decb2346774d45a3e5af40b2
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-09-08 00:25:13 -0700

    Simplify colorscheme declaration

--- neovim/.config/nvim/lua/config/theme.lua
+++ neovim/.config/nvim/lua/config/theme.lua
@@ -1,8 +1,3 @@
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-
--- https://github.com/neovim/neovim/issues/18201#issuecomment-1382075049
-vim.api.nvim_cmd({
-	cmd = "colorscheme",
-	args = { "tokyonight" },
-}, {})
+vim.cmd([[colorscheme tokyonight-storm]])
