02399826698c01931e43e38df17ced9d65a22858
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-09-07 17:19:43 -0700

    Only enable tpope fugitive when inside a git repo

--- neovim/.config/nvim/lua/plugins/tpope.lua
+++ neovim/.config/nvim/lua/plugins/tpope.lua
@@ -1,7 +1,11 @@
 return {
 	{
 		"tpope/vim-fugitive",
-		lazy = false,
+		event = { "VeryLazy" },
+		cond = function()
+			local output = vim.fn.systemlist("git rev-parse --is-inside-work-tree 2>/dev/null")
+			return #output ~= 0
+		end,
 	},
 	{
 		"tpope/vim-unimpaired",
