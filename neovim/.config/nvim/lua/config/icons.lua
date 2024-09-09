89ba996d4e31fa5bc046cad4c759b917bbbec479
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-09-08 20:27:59 -0700

    Disable strict icon matching by filename to see if I notice

--- neovim/.config/nvim/lua/config/icons.lua
+++ neovim/.config/nvim/lua/config/icons.lua
@@ -18,7 +18,7 @@ require("nvim-web-devicons").setup({
 	-- different tables, first by filename, and if not found by extension; this
 	-- prevents cases when file doesn't have any extension but still gets some icon
 	-- because its name happened to match some extension (default to false)
-	strict = true,
+	strict = false,
 
 	-- globally enable different highlight colors per icon (default to true)
 	-- if set to false all icons will have the default icon's color
