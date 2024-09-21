05186452a3cae49c23170fdf9a8f79d7c0ca24fb
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-09-21 01:50:41 -0700

    Automatically get ft list for conform

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -1,35 +1,46 @@
--- conform replaces null-ls for formatting different languages
+local formatters_by_ft = {
+	css = { "prettier" },
+	go = {
+		"gofumpt",
+		"goimports",
+		"golines",
+	},
+	html = { "prettier" },
+	javascript = { "prettier" },
+	lua = { "stylua" },
+	python = {
+		"black",
+		"isort",
+		"pydocstyle",
+	},
+	terraform = { "terraform_fmt" },
+	sass = { "prettier" },
+	sh = {
+		"shellharden",
+		"shfmt",
+	},
+	yaml = { "yamlfmt" },
+}
+
+local function table_keys(t)
+	local keys = {}
+	for k, _ in pairs(t) do
+		table.insert(keys, k)
+	end
+	return keys
+end
+
+-- Get the list of filetypes from formatters_by_ft
+local ft = table_keys(formatters_by_ft)
+
 return {
 	"stevearc/conform.nvim",
 	opts = {},
-	ft = {
-		"css",
-		"go",
-		"html",
-		"javascript",
-		"lua",
-		"python",
-		"terraform",
-		"sh",
-		"yaml",
-	},
+	ft = ft,
 	config = function()
 		require("conform").setup({
-			formatters_by_ft = {
-				css = { "prettier" },
-				go = { "gofumpt", "goimports", "golines" },
-				html = { "prettier" },
-				javascript = { "prettier" },
-				lua = { "stylua" },
-				python = { "isort", "black", "pydocstyle" },
-				terraform = { "terraform_fmt" },
-				sass = { "prettier" },
-				sh = { "shellharden", "shfmt" },
-				yaml = { "yamlfmt" },
-			},
-
+			formatters_by_ft = formatters_by_ft,
 			format_on_save = {
-				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				lsp_fallback = true,
 			},
