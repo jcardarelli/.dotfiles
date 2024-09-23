d28d505b0af3bc7db806aff71346122cf87fc563
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-09-23 00:09:50 -0700

    Use vsplit instead of split for nvr GIT_EDITOR command

--- neovim/.config/nvim/lua/config/globals.lua
+++ neovim/.config/nvim/lua/config/globals.lua
@@ -2,7 +2,7 @@
 --- Environment variables
 --------------------------------------------------------------------------------
 -- https://github.com/mhinz/neovim-remote?tab=readme-ov-file#typical-use-cases
-vim.env.GIT_EDITOR = "nvr -cc split --remote-wait"
+vim.env.GIT_EDITOR = "nvr -cc vsplit --remote-wait"
 
 --------------------------------------------------------------------------------
 -- Globals
