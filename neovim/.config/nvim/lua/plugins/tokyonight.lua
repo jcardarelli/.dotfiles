37471a8787813f8c4a606325d4a992e13ef7fa50
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-09-25 04:55:52 -0700

    Rewrite tokyonight neovim config

--- neovim/.config/nvim/lua/plugins/tokyonight.lua
+++ neovim/.config/nvim/lua/plugins/tokyonight.lua
@@ -2,9 +2,9 @@ return {
 	-- Colorscheme
 	"folke/tokyonight.nvim",
 	opts = {
-		style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+		style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
 		light_style = "day", -- The theme is used when the background is set to light
-		transparent = true, -- Enable this to disable setting the background color
+		transparent = false, -- Enable this to disable setting the background color
 		terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
 		styles = {
 			-- Style to be applied to different syntax groups
@@ -17,11 +17,35 @@ return {
 			sidebars = "dark", -- style for sidebars, see below
 			floats = "dark", -- style for floating windows
 		},
-		cache = true,
-		-- sidebars = { "qf", "help", "CHADtree", "neoterm" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
 		day_brightness = 0.4, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-		hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
 		dim_inactive = false, -- dims inactive windows
 		lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+
+		--- You can override specific color groups to use other groups or a hex color
+		--- function will be called with a ColorScheme table
+		---@param colors ColorScheme
+		on_colors = function(colors) end,
+
+		--- You can override specific highlights to use other groups or a hex color
+		--- function will be called with a Highlights and ColorScheme table
+		---@param highlights tokyonight.Highlights
+		---@param colors ColorScheme
+		on_highlights = function(highlights, colors) end,
+
+		cache = true, -- When set to true, the theme will be cached for better performance
+
+		---@type table<string, boolean|{enabled:boolean}>
+		plugins = {
+			-- enable all plugins when not using lazy.nvim
+			-- set to false to manually enable/disable plugins
+			all = package.loaded.lazy == nil,
+			-- uses your plugin manager to automatically enable needed plugins
+			-- currently only lazy.nvim is supported
+			auto = true,
+			-- add any plugins here that you want to enable
+			-- for all possible plugins, see:
+			--   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+			-- telescope = true,
+		},
 	},
 }
