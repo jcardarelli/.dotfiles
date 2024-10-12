894b38f88a24f40e6ac0d401bf309a606e91be5c
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-10-11 21:49:20 -0700

    Replace nvim-cmp nvim_lsp_signature_help with lsp_signature

--- neovim/.config/nvim/lua/config/nvim-cmp.lua
+++ neovim/.config/nvim/lua/config/nvim-cmp.lua
@@ -81,7 +81,6 @@ cmp.setup({
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "nvim_lsp_signature_help" },
 		{ name = "nvim_lua" },
 		{ name = "cmdline" },
 		{ name = "luasnip" },
